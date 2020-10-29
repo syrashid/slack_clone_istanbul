45e1d47e5739cc6cbd64bb7dfec0c53d1537acb5
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-29 16:46:33 +0300

    FEAT: Added video chat capabilities per chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,5 +2,9 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+
+    opentok = OpenTok::OpenTok.new(ENV["VONAGE_API_KEY"], ENV["VONAGE_API_SECRET"])
+    @token = opentok.generate_token @chatroom.vonage_session_id, { name: current_user.nickname }
+
   end
 end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,3 +1,8 @@
 class Chatroom < ApplicationRecord
+  before_create do
+    opentok = OpenTok::OpenTok.new(ENV["VONAGE_API_KEY"], ENV["VONAGE_API_SECRET"])
+    session = opentok.create_session
+    self.vonage_session_id = session.session_id
+  end
   has_many :messages
 end
